99bdc1b52ae9b26fef474dbeeaf0dbce461fbee4
ezekiel shapiro arin <ezekielarin@gmail.com>
2019-08-24 00:50:13 +0100

    Update login.php

--- application/views/login.php
+++ application/views/login.php
@@ -5,20 +5,37 @@
 </head>
 <body>
 
-<div>
-	<p>
-		<a href="logout">logout</a>
-		
-	</p>
+<div class="container">
+   <div class="row">
 
-<form method="post">
-	<label>email</label>
-	<input type="text" name="username"><br>
-	<label>password</label>
-	<input type="password" name="password"><br>
-	<input type="checkbox" name="remember" value="1"> Remember me<br>
-	<input type="submit" name="login" value="login">
-</form>
+		<form method="post">
+			<label>email</label>
+			<input type="text" name="username"><br>
+			<label>password</label>
+			<input type="password" name="password"><br>
+			<input type="checkbox" name="remember" value="1"> Remember me<br>
+			<input type="submit" name="login" value="login">	
+		</form>
+    <div>
+		<a href="forgot_password/">Forgot password</a>
+		<a href="register">Signup</a>
+	</div>
 </div>
+<style type="text/css">
+	.container{
+      position: center;
+      padding: 20px;
+      align-content: center;
+      overflow: left;
+	}
+	.row{
+       border: 2px;
+
+	}
+	.container .row{
+
+	}
+</style>
+
 </body>
 </html>
