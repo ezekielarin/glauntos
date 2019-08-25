1293455c606e90ada44389cbfdfe545f12ada4eb
ezekiel shapiro arin <ezekielarin@gmail.com>
2019-08-25 21:05:43 +0100

    Update edit_profile.php

--- application/views/edit_profile.php
+++ application/views/edit_profile.php
@@ -12,14 +12,23 @@
 <div>
 	<div>
 		<form method="post">
-			<label>first name</label>
-			<input type="hidden" name="username" value="<?php echo $user->username?>"><br>
+			<input type="hidden" name="username" value="<?php echo $user->username?>">
+			<div>
+				<label>first name</label>
 			<input type="text" name="first_name" value="<?php echo $user->first_name?>"><br>
-            <label>last name</label>
-			<input type="text" name="last_name" value="<?php echo $user->last_name?>"><br>
-			<label>phone</label>
-			<input type="text" name="phone" value="<?php echo $user->phone?>"><br>
-			<input type="submit" name="update" value="update">
+			</div>
+			<div>
+				<label>last name</label>
+		    	<input type="text" name="last_name" value="<?php echo $user->last_name?>"><br>
+			</div>
+            <div>
+            	<label>phone</label>
+			    <input type="text" name="phone" value="<?php echo $user->phone?>"><br>
+            </div>
+			<div>
+				<input type="submit" name="update" value="update">
+			</div>
+			
 		</form>
 	</div>
 </div>
