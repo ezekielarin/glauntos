c2169de933bb146c53be8fe621f73dacfc775b31
ezekiel shapiro arin <ezekielarin@gmail.com>
2019-09-19 11:07:16 +0100

    Create log-2019-09-11.php

---
+++ application/logs/log-2019-09-11.php
@@ -0,0 +1,80 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2019-09-11 19:15:48 --> Config Class Initialized
+INFO - 2019-09-11 19:15:48 --> Hooks Class Initialized
+DEBUG - 2019-09-11 19:15:48 --> UTF-8 Support Enabled
+INFO - 2019-09-11 19:15:48 --> Utf8 Class Initialized
+INFO - 2019-09-11 19:15:48 --> URI Class Initialized
+INFO - 2019-09-11 19:15:48 --> Router Class Initialized
+INFO - 2019-09-11 19:15:49 --> Output Class Initialized
+INFO - 2019-09-11 19:15:49 --> Security Class Initialized
+DEBUG - 2019-09-11 19:15:49 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-09-11 19:15:49 --> Input Class Initialized
+INFO - 2019-09-11 19:15:49 --> Language Class Initialized
+INFO - 2019-09-11 19:15:49 --> Loader Class Initialized
+INFO - 2019-09-11 19:15:50 --> Database Driver Class Initialized
+DEBUG - 2019-09-11 19:15:50 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2019-09-11 19:15:51 --> Session: Class initialized using 'files' driver.
+INFO - 2019-09-11 19:15:51 --> Controller Class Initialized
+INFO - 2019-09-11 19:15:51 --> Helper loaded: url_helper
+INFO - 2019-09-11 19:15:51 --> Helper loaded: date_helper
+INFO - 2019-09-11 19:15:51 --> Helper loaded: string_helper
+INFO - 2019-09-11 19:15:51 --> Model "Usermodel" initialized
+INFO - 2019-09-11 19:15:51 --> Helper loaded: cookie_helper
+INFO - 2019-09-11 19:15:52 --> Config Class Initialized
+INFO - 2019-09-11 19:15:52 --> Hooks Class Initialized
+DEBUG - 2019-09-11 19:15:52 --> UTF-8 Support Enabled
+INFO - 2019-09-11 19:15:52 --> Utf8 Class Initialized
+INFO - 2019-09-11 19:15:52 --> URI Class Initialized
+INFO - 2019-09-11 19:15:52 --> Router Class Initialized
+INFO - 2019-09-11 19:15:53 --> Output Class Initialized
+INFO - 2019-09-11 19:15:53 --> Security Class Initialized
+DEBUG - 2019-09-11 19:15:53 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-09-11 19:15:53 --> Input Class Initialized
+INFO - 2019-09-11 19:15:53 --> Language Class Initialized
+INFO - 2019-09-11 19:15:53 --> Loader Class Initialized
+INFO - 2019-09-11 19:15:53 --> Database Driver Class Initialized
+DEBUG - 2019-09-11 19:15:53 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2019-09-11 19:15:54 --> Session: Class initialized using 'files' driver.
+INFO - 2019-09-11 19:15:54 --> Controller Class Initialized
+INFO - 2019-09-11 19:15:54 --> Helper loaded: url_helper
+INFO - 2019-09-11 19:15:54 --> Helper loaded: date_helper
+INFO - 2019-09-11 19:15:54 --> Helper loaded: string_helper
+INFO - 2019-09-11 19:15:54 --> Model "Usermodel" initialized
+INFO - 2019-09-11 19:15:54 --> Helper loaded: cookie_helper
+INFO - 2019-09-11 19:15:54 --> File loaded: C:\wamp\www\cauth\application\views\login.php
+INFO - 2019-09-11 19:15:54 --> Final output sent to browser
+DEBUG - 2019-09-11 19:15:55 --> Total execution time: 2.5748
+INFO - 2019-09-11 19:16:07 --> Config Class Initialized
+INFO - 2019-09-11 19:16:07 --> Hooks Class Initialized
+DEBUG - 2019-09-11 19:16:07 --> UTF-8 Support Enabled
+INFO - 2019-09-11 19:16:07 --> Utf8 Class Initialized
+INFO - 2019-09-11 19:16:08 --> URI Class Initialized
+INFO - 2019-09-11 19:16:08 --> Router Class Initialized
+INFO - 2019-09-11 19:16:08 --> Output Class Initialized
+INFO - 2019-09-11 19:16:08 --> Security Class Initialized
+DEBUG - 2019-09-11 19:16:08 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-09-11 19:16:08 --> Input Class Initialized
+INFO - 2019-09-11 19:16:08 --> Language Class Initialized
+ERROR - 2019-09-11 19:16:08 --> 404 Page Not Found: dashboard//index
+INFO - 2019-09-11 19:17:08 --> Config Class Initialized
+INFO - 2019-09-11 19:17:08 --> Hooks Class Initialized
+DEBUG - 2019-09-11 19:17:08 --> UTF-8 Support Enabled
+INFO - 2019-09-11 19:17:08 --> Utf8 Class Initialized
+INFO - 2019-09-11 19:17:08 --> URI Class Initialized
+DEBUG - 2019-09-11 19:17:08 --> No URI present. Default controller set.
+INFO - 2019-09-11 19:17:08 --> Router Class Initialized
+INFO - 2019-09-11 19:17:08 --> Output Class Initialized
+INFO - 2019-09-11 19:17:09 --> Security Class Initialized
+DEBUG - 2019-09-11 19:17:09 --> Global POST, GET and COOKIE data sanitized
+INFO - 2019-09-11 19:17:09 --> Input Class Initialized
+INFO - 2019-09-11 19:17:09 --> Language Class Initialized
+INFO - 2019-09-11 19:17:09 --> Loader Class Initialized
+INFO - 2019-09-11 19:17:09 --> Database Driver Class Initialized
+DEBUG - 2019-09-11 19:17:09 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2019-09-11 19:17:09 --> Session: Class initialized using 'files' driver.
+INFO - 2019-09-11 19:17:09 --> Controller Class Initialized
+INFO - 2019-09-11 19:17:09 --> Helper loaded: url_helper
+INFO - 2019-09-11 19:17:09 --> Helper loaded: date_helper
+INFO - 2019-09-11 19:17:09 --> Helper loaded: string_helper
+INFO - 2019-09-11 19:17:09 --> Model "Usermodel" initialized
